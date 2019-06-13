9c487d95e13abe35b49465466b9957c4a1e4de4f
re-arep <re-arep@gmail.com>
2019-06-13 17:23:55 +0900

    2019-06-13

--- RNAP_def_3.py
+++ RNAP_def_3.py
@@ -21,7 +21,7 @@ class RNAP:
             return False
 
     def find_fi_code(self):
-        if self.fi_code in self.dna[self.find_in_code():]:
+        if self.fi_code in self.dna[self.find_in_code()+1:]:
             return self.dna.index(self.fi_code)
         else:
             return False
@@ -30,7 +30,7 @@ class RNAP:
         fic = self.find_in_code()
         ffc = self.find_fi_code()
         direct = ["3'", "5'"]
-        if fic != False and ffc != False:
+        if (fic != False) and (ffc != False):
             for i in range(fic, ffc+len(self.fi_code)):
                 if self.dna[i] == "A":
                     self.rna += "U"
@@ -44,8 +44,6 @@ class RNAP:
                 reversed(self.rna)
             else:
                 reversed(direct)
-            return "DNA mode :"+str(self.dna_mode)+"\n"+"접합 지점 :"+str(self.pos)+"\n"+ \
-                   "전사된 RNA :\n"+direct[0]+"->"+self.rna+"->"+direct[1]
+            return str(self.dna_mode), str(self.pos), direct[0], self.rna, direct[1]
         else:
-            return "RNA가 합성될 수 없습니다\n"+"DNA mode :"+str(self.dna_mode)+"\n"+"접합 지점 :"+str(self.pos)+ \
-                   "\n"+"개시코드존재 :"+str(fic)+"번째,"+"종결코드존재 :"+str(ffc)+"번째"
+            return False
